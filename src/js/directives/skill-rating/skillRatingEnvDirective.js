fe99421fcc90e301ef100087d34fcc5e1f93ccde
bbffbb <birkirfb13@ru.is>
2016-11-28 21:29:37 +0000

    dialogs refactored

---
+++ src/js/directives/skill-rating/skillRatingEnvDirective.js
@@ -0,0 +1,10 @@
+
+angular.module('ChatApp').directive('skillRatingEnv', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/directives/skill-rating/skill-rating-env.html',
+        link: function (scope, element, attrs) {
+            //funct  
+        } 
+    };
+});
---
+++ src/js/directives/skill-rating/skillRatingLangDirective.js
@@ -0,0 +1,10 @@
+
+angular.module('ChatApp').directive('skillRatingLang', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/directives/skill-rating/skill-rating-lang.html',
+        link: function (scope, element, attrs) {
+            //funct  
+        } 
+    };
+});
